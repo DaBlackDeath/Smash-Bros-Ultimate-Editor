89ca355f30d77bc1178b68a5d9b5e0c9e15e7757
BlackDeath <dablackdeath@gmail.com>
2018-11-28 16:17:09 +0100

    new upload

---
+++ SSBU_Switch_Editor/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace SSBU_Switch_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.open_file = new System.Windows.Forms.Button();
+            this.gold_box = new System.Windows.Forms.TextBox();
+            this.sp_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tickets_box = new System.Windows.Forms.TextBox();
+            this.Snack_S_Box = new System.Windows.Forms.TextBox();
+            this.Snack_M_Box = new System.Windows.Forms.TextBox();
+            this.Snack_L_Box = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Hammers_Box = new System.Windows.Forms.TextBox();
+            this.SkillPoints_Box = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.unlock_spirits_checkbox = new System.Windows.Forms.CheckBox();
+            this.unlock_cores_checkbox = new System.Windows.Forms.CheckBox();
+            this.unlock_roster_checkbox = new System.Windows.Forms.CheckBox();
+            this.save_file = new System.Windows.Forms.Button();
+            this.max_all = new System.Windows.Forms.Button();
+            this.mass_unlock_checkbox = new System.Windows.Forms.CheckBox();
+            this.unlock_milestones_checkbox = new System.Windows.Forms.CheckBox();
+            this.unlock_miiclothes_checkbox = new System.Windows.Forms.CheckBox();
+            this.unlock_stickers_checkbox = new System.Windows.Forms.CheckBox();
+            this.unlock_fullsize_checkbox = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // open_file
+            // 
+            this.open_file.Location = new System.Drawing.Point(9, 13);
+            this.open_file.Name = "open_file";
+            this.open_file.Size = new System.Drawing.Size(477, 23);
+            this.open_file.TabIndex = 0;
+            this.open_file.Text = "open system_data.bin";
+            this.open_file.UseVisualStyleBackColor = true;
+            this.open_file.Click += new System.EventHandler(this.open_file_Click);
+            // 
+            // gold_box
+            // 
+            this.gold_box.Location = new System.Drawing.Point(86, 41);
+            this.gold_box.MaxLength = 7;
+            this.gold_box.Name = "gold_box";
+            this.gold_box.Size = new System.Drawing.Size(99, 20);
+            this.gold_box.TabIndex = 1;
+            // 
+            // sp_box
+            // 
+            this.sp_box.Location = new System.Drawing.Point(86, 67);
+            this.sp_box.MaxLength = 7;
+            this.sp_box.Name = "sp_box";
+            this.sp_box.Size = new System.Drawing.Size(99, 20);
+            this.sp_box.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Gold";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "SP";
+            // 
+            // tickets_box
+            // 
+            this.tickets_box.Location = new System.Drawing.Point(86, 93);
+            this.tickets_box.MaxLength = 2;
+            this.tickets_box.Name = "tickets_box";
+            this.tickets_box.Size = new System.Drawing.Size(99, 20);
+            this.tickets_box.TabIndex = 6;
+            // 
+            // Snack_S_Box
+            // 
+            this.Snack_S_Box.Location = new System.Drawing.Point(390, 41);
+            this.Snack_S_Box.MaxLength = 5;
+            this.Snack_S_Box.Name = "Snack_S_Box";
+            this.Snack_S_Box.Size = new System.Drawing.Size(99, 20);
+            this.Snack_S_Box.TabIndex = 7;
+            // 
+            // Snack_M_Box
+            // 
+            this.Snack_M_Box.Location = new System.Drawing.Point(390, 67);
+            this.Snack_M_Box.MaxLength = 5;
+            this.Snack_M_Box.Name = "Snack_M_Box";
+            this.Snack_M_Box.Size = new System.Drawing.Size(99, 20);
+            this.Snack_M_Box.TabIndex = 8;
+            // 
+            // Snack_L_Box
+            // 
+            this.Snack_L_Box.Location = new System.Drawing.Point(390, 93);
+            this.Snack_L_Box.MaxLength = 5;
+            this.Snack_L_Box.Name = "Snack_L_Box";
+            this.Snack_L_Box.Size = new System.Drawing.Size(99, 20);
+            this.Snack_L_Box.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Tickets";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(316, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Snacks [S]";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(316, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Snacks [M]";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(316, 96);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Snacks [L]";
+            // 
+            // Hammers_Box
+            // 
+            this.Hammers_Box.Location = new System.Drawing.Point(86, 119);
+            this.Hammers_Box.MaxLength = 2;
+            this.Hammers_Box.Name = "Hammers_Box";
+            this.Hammers_Box.Size = new System.Drawing.Size(99, 20);
+            this.Hammers_Box.TabIndex = 14;
+            // 
+            // SkillPoints_Box
+            // 
+            this.SkillPoints_Box.Location = new System.Drawing.Point(390, 119);
+            this.SkillPoints_Box.MaxLength = 7;
+            this.SkillPoints_Box.Name = "SkillPoints_Box";
+            this.SkillPoints_Box.Size = new System.Drawing.Size(99, 20);
+            this.SkillPoints_Box.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 122);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Hammers";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(316, 122);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Skill Points";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.unlock_fullsize_checkbox);
+            this.groupBox1.Controls.Add(this.unlock_stickers_checkbox);
+            this.groupBox1.Controls.Add(this.unlock_miiclothes_checkbox);
+            this.groupBox1.Controls.Add(this.unlock_milestones_checkbox);
+            this.groupBox1.Controls.Add(this.mass_unlock_checkbox);
+            this.groupBox1.Controls.Add(this.unlock_spirits_checkbox);
+            this.groupBox1.Controls.Add(this.unlock_cores_checkbox);
+            this.groupBox1.Controls.Add(this.unlock_roster_checkbox);
+            this.groupBox1.Location = new System.Drawing.Point(15, 177);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(471, 113);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            // 
+            // unlock_spirits_checkbox
+            // 
+            this.unlock_spirits_checkbox.AutoSize = true;
+            this.unlock_spirits_checkbox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.unlock_spirits_checkbox.Location = new System.Drawing.Point(349, 42);
+            this.unlock_spirits_checkbox.Name = "unlock_spirits_checkbox";
+            this.unlock_spirits_checkbox.Size = new System.Drawing.Size(105, 17);
+            this.unlock_spirits_checkbox.TabIndex = 2;
+            this.unlock_spirits_checkbox.Text = "Unlock All Spirits";
+            this.unlock_spirits_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // unlock_cores_checkbox
+            // 
+            this.unlock_cores_checkbox.AutoSize = true;
+            this.unlock_cores_checkbox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.unlock_cores_checkbox.Location = new System.Drawing.Point(350, 19);
+            this.unlock_cores_checkbox.Name = "unlock_cores_checkbox";
+            this.unlock_cores_checkbox.Size = new System.Drawing.Size(104, 17);
+            this.unlock_cores_checkbox.TabIndex = 1;
+            this.unlock_cores_checkbox.Text = "Unlock All Cores";
+            this.unlock_cores_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // unlock_roster_checkbox
+            // 
+            this.unlock_roster_checkbox.AutoSize = true;
+            this.unlock_roster_checkbox.Location = new System.Drawing.Point(11, 65);
+            this.unlock_roster_checkbox.Name = "unlock_roster_checkbox";
+            this.unlock_roster_checkbox.Size = new System.Drawing.Size(162, 17);
+            this.unlock_roster_checkbox.TabIndex = 0;
+            this.unlock_roster_checkbox.Text = "Unlock Full Character Roster";
+            this.unlock_roster_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // save_file
+            // 
+            this.save_file.Location = new System.Drawing.Point(9, 308);
+            this.save_file.Name = "save_file";
+            this.save_file.Size = new System.Drawing.Size(477, 23);
+            this.save_file.TabIndex = 19;
+            this.save_file.Text = "save system_data.bin";
+            this.save_file.UseVisualStyleBackColor = true;
+            this.save_file.Click += new System.EventHandler(this.save_file_Click);
+            // 
+            // max_all
+            // 
+            this.max_all.Location = new System.Drawing.Point(414, 148);
+            this.max_all.Name = "max_all";
+            this.max_all.Size = new System.Drawing.Size(75, 23);
+            this.max_all.TabIndex = 20;
+            this.max_all.Text = "Max All";
+            this.max_all.UseVisualStyleBackColor = true;
+            this.max_all.Click += new System.EventHandler(this.max_all_Click);
+            // 
+            // mass_unlock_checkbox
+            // 
+            this.mass_unlock_checkbox.AutoSize = true;
+            this.mass_unlock_checkbox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.mass_unlock_checkbox.Location = new System.Drawing.Point(217, 88);
+            this.mass_unlock_checkbox.Name = "mass_unlock_checkbox";
+            this.mass_unlock_checkbox.Size = new System.Drawing.Size(237, 17);
+            this.mass_unlock_checkbox.TabIndex = 3;
+            this.mass_unlock_checkbox.Text = "Unlock All Stores, Explore, Gym, Dojo, Music";
+            this.mass_unlock_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // unlock_milestones_checkbox
+            // 
+            this.unlock_milestones_checkbox.AutoSize = true;
+            this.unlock_milestones_checkbox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.unlock_milestones_checkbox.Location = new System.Drawing.Point(327, 65);
+            this.unlock_milestones_checkbox.Name = "unlock_milestones_checkbox";
+            this.unlock_milestones_checkbox.Size = new System.Drawing.Size(127, 17);
+            this.unlock_milestones_checkbox.TabIndex = 4;
+            this.unlock_milestones_checkbox.Text = "Unlock All Milestones";
+            this.unlock_milestones_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // unlock_miiclothes_checkbox
+            // 
+            this.unlock_miiclothes_checkbox.AutoSize = true;
+            this.unlock_miiclothes_checkbox.Location = new System.Drawing.Point(11, 88);
+            this.unlock_miiclothes_checkbox.Name = "unlock_miiclothes_checkbox";
+            this.unlock_miiclothes_checkbox.Size = new System.Drawing.Size(128, 17);
+            this.unlock_miiclothes_checkbox.TabIndex = 5;
+            this.unlock_miiclothes_checkbox.Text = "Unlock All Mii Clothes";
+            this.unlock_miiclothes_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // unlock_stickers_checkbox
+            // 
+            this.unlock_stickers_checkbox.AutoSize = true;
+            this.unlock_stickers_checkbox.Location = new System.Drawing.Point(11, 19);
+            this.unlock_stickers_checkbox.Name = "unlock_stickers_checkbox";
+            this.unlock_stickers_checkbox.Size = new System.Drawing.Size(216, 17);
+            this.unlock_stickers_checkbox.TabIndex = 6;
+            this.unlock_stickers_checkbox.Text = "Unlock All Spirits and Collection Stickers";
+            this.unlock_stickers_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // unlock_fullsize_checkbox
+            // 
+            this.unlock_fullsize_checkbox.AutoSize = true;
+            this.unlock_fullsize_checkbox.Location = new System.Drawing.Point(11, 42);
+            this.unlock_fullsize_checkbox.Name = "unlock_fullsize_checkbox";
+            this.unlock_fullsize_checkbox.Size = new System.Drawing.Size(159, 17);
+            this.unlock_fullsize_checkbox.TabIndex = 7;
+            this.unlock_fullsize_checkbox.Text = "Unlock Full Size Spirit Board";
+            this.unlock_fullsize_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 342);
+            this.Controls.Add(this.max_all);
+            this.Controls.Add(this.save_file);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.SkillPoints_Box);
+            this.Controls.Add(this.Hammers_Box);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Snack_L_Box);
+            this.Controls.Add(this.Snack_M_Box);
+            this.Controls.Add(this.Snack_S_Box);
+            this.Controls.Add(this.tickets_box);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sp_box);
+            this.Controls.Add(this.gold_box);
+            this.Controls.Add(this.open_file);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Super Smash Bros. Ultimate Savegame Editor";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open_file;
+        private System.Windows.Forms.TextBox gold_box;
+        private System.Windows.Forms.TextBox sp_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tickets_box;
+        private System.Windows.Forms.TextBox Snack_S_Box;
+        private System.Windows.Forms.TextBox Snack_M_Box;
+        private System.Windows.Forms.TextBox Snack_L_Box;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Hammers_Box;
+        private System.Windows.Forms.TextBox SkillPoints_Box;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox unlock_cores_checkbox;
+        private System.Windows.Forms.CheckBox unlock_roster_checkbox;
+        private System.Windows.Forms.Button save_file;
+        private System.Windows.Forms.CheckBox unlock_spirits_checkbox;
+        private System.Windows.Forms.Button max_all;
+        private System.Windows.Forms.CheckBox mass_unlock_checkbox;
+        private System.Windows.Forms.CheckBox unlock_milestones_checkbox;
+        private System.Windows.Forms.CheckBox unlock_miiclothes_checkbox;
+        private System.Windows.Forms.CheckBox unlock_stickers_checkbox;
+        private System.Windows.Forms.CheckBox unlock_fullsize_checkbox;
+    }
+}
+
---
+++ SSBU_Switch_Editor/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SSBU_Switch_Editor
+{
+    public partial class Form1 : Form
+    {
+        public string Savegame;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static byte[] BLKDTH_STR2BA(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                .ToArray();
+        }
+
+        public void BLKDTH_PatchFile(string filename, int offset, byte[] value)
+        {
+            var fs = new FileStream(filename, FileMode.Open);
+            var br = new BinaryWriter(fs);
+            fs.Position = offset;
+            br.Write(value);
+            fs.Close();
+            br.Close();
+        }
+
+        private void BLKDTH_get_openfile()
+        {
+            var openFd = new OpenFileDialog();
+            if (openFd.ShowDialog() == DialogResult.OK) Savegame = openFd.FileName;
+        }
+
+        private void BLKDTH_get_data()
+        {
+            {
+                var savegameFs = new FileStream(Savegame, FileMode.Open);
+                var savegameBr = new BinaryReader(savegameFs);
+                var length = savegameFs.Length;
+
+                // Gold
+                savegameBr.BaseStream.Position = offsets.Gold;
+                gold_box.Text = BitConverter.ToUInt32(savegameBr.ReadBytes(4), 0).ToString();
+
+                // SP
+                savegameBr.BaseStream.Position = offsets.Sp;
+                sp_box.Text = BitConverter.ToUInt32(savegameBr.ReadBytes(4), 0).ToString();
+
+                // Tickets
+                savegameBr.BaseStream.Position = offsets.Tickets;
+                tickets_box.Text = int.Parse(BitConverter.ToString(savegameBr.ReadBytes(1), 0), NumberStyles.HexNumber).ToString();
+
+                // Snacks [S]
+                savegameBr.BaseStream.Position = offsets.Snack[0];
+                Snack_S_Box.Text = BitConverter.ToUInt16(savegameBr.ReadBytes(2), 0).ToString();
+
+                // Snacks [M]
+                savegameBr.BaseStream.Position = offsets.Snack[1];
+                Snack_M_Box.Text = BitConverter.ToUInt16(savegameBr.ReadBytes(2), 0).ToString();
+
+                // Snacks [L]
+                savegameBr.BaseStream.Position = offsets.Snack[2];
+                Snack_L_Box.Text = BitConverter.ToUInt16(savegameBr.ReadBytes(2), 0).ToString();
+
+                // Hammers
+                savegameBr.BaseStream.Position = offsets.Hammers;
+                Hammers_Box.Text = int.Parse(BitConverter.ToString(savegameBr.ReadBytes(1), 0), NumberStyles.HexNumber).ToString();
+
+                // Skill Points
+                savegameBr.BaseStream.Position = offsets.SkillPoints;
+                SkillPoints_Box.Text = BitConverter.ToUInt16(savegameBr.ReadBytes(2), 0).ToString();
+
+                savegameBr.Close();
+            }
+        }
+
+        private void BLKDTH_set_data()
+        {
+            FileStream updateSaveOpen = null;
+            BinaryWriter updateSaveWrite = null;
+            updateSaveOpen = new FileStream(Savegame, FileMode.Open);
+            updateSaveWrite = new BinaryWriter(updateSaveOpen);
+            
+            //Gold
+            updateSaveOpen.Position = offsets.Gold;
+            updateSaveWrite.Write(BitConverter.GetBytes(uint.Parse(gold_box.Text)));
+
+            //SP
+            updateSaveOpen.Position = offsets.Sp;
+            updateSaveWrite.Write(BitConverter.GetBytes(uint.Parse(sp_box.Text)));
+
+            //Tickets
+            updateSaveOpen.Position = offsets.Tickets;
+            updateSaveWrite.Write(BitConverter.GetBytes(uint.Parse(tickets_box.Text)));
+
+            //Snacks [S]
+            updateSaveOpen.Position = offsets.Snack[0];
+            updateSaveWrite.Write(BitConverter.GetBytes(ushort.Parse(Snack_S_Box.Text)));
+
+            //Snacks [M]
+            updateSaveOpen.Position = offsets.Snack[1];
+            updateSaveWrite.Write(BitConverter.GetBytes(ushort.Parse(Snack_M_Box.Text)));
+
+            //Snacks [L]
+            updateSaveOpen.Position = offsets.Snack[2];
+            updateSaveWrite.Write(BitConverter.GetBytes(ushort.Parse(Snack_L_Box.Text)));
+
+            //Hammers
+            updateSaveOpen.Position = offsets.Hammers;
+            updateSaveWrite.Write(BLKDTH_STR2BA(int.Parse(Hammers_Box.Text).ToString("X2")));
+
+            //Skill Points
+            updateSaveOpen.Position = offsets.SkillPoints;
+            updateSaveWrite.Write(BitConverter.GetBytes(uint.Parse(SkillPoints_Box.Text))); 
+            
+            updateSaveOpen.Close();
+        }
+
+        private void BLKDTH_check_max()
+        {
+            if (int.Parse(SkillPoints_Box.Text) > 3000)
+            {
+                SkillPoints_Box.Text = "3000";
+            }
+            if (int.Parse(Snack_S_Box.Text) > 65535)
+            {
+                Snack_S_Box.Text = "65535";
+            }
+            if (int.Parse(Snack_M_Box.Text) > 65535)
+            {
+                Snack_M_Box.Text = "65535";
+            }
+            if (int.Parse(Snack_L_Box.Text) > 65535)
+            {
+                Snack_L_Box.Text = "65535";
+            } 
+
+        }
+
+        private void mass_unlock()
+        {
+            byte[] b = { 0xFF };
+            for (int i = 0; i < 0x2A4; i++)
+            {
+                BLKDTH_PatchFile(Savegame, 0 + i, b);
+            }
+        }
+
+        private void milestones_unlock()
+        {
+            byte[] b = { 0xFF };
+            for (int i = 0; i < 0x30; i++)
+            {
+                BLKDTH_PatchFile(Savegame, 0 + i, b);
+            }
+        }
+
+        private void open_file_Click(object sender, EventArgs e)
+        {
+            BLKDTH_get_openfile();
+            if (string.IsNullOrEmpty(Savegame))
+            {
+                MessageBox.Show("no savegame selected");
+            }
+            else
+            {
+                BLKDTH_get_data();
+            }
+
+
+        }
+
+        private void save_file_Click(object sender, EventArgs e)
+        {
+             
+            BLKDTH_check_max();
+            BLKDTH_set_data();
+            if (unlock_roster_checkbox.Checked == true)
+            {
+                BLKDTH_PatchFile(Savegame, offsets.Roster, offsets.UnlockChars);
+            }
+            if (unlock_cores_checkbox.Checked == true)
+            {
+                BLKDTH_PatchFile(Savegame, offsets.Cores, offsets.UnlockCores);
+            }
+             if (unlock_spirits_checkbox.Checked == true)
+             {
+                 BLKDTH_PatchFile(Savegame, offsets.Spirits, offsets.UnlockSpirits);
+             }
+             if (mass_unlock_checkbox.Checked == true)
+             {
+                 mass_unlock();
+             }
+             if (unlock_milestones_checkbox.Checked == true)
+             {
+                 milestones_unlock();
+             }
+             if (unlock_miiclothes_checkbox.Checked == true)
+             {
+                 BLKDTH_PatchFile(Savegame, offsets.MiiClothes, offsets.UnlockMiiClothes);
+             }
+             if (unlock_fullsize_checkbox.Checked == true)
+             {
+                 BLKDTH_PatchFile(Savegame, offsets.FullSizeSpiritBoard, offsets.UnlockFullSizeSpiritBoard);
+             }
+             if (unlock_stickers_checkbox.Checked == true)
+             {
+                 BLKDTH_PatchFile(Savegame, offsets.AllSpiritsandCollectionStickers, offsets.UnlockAllSpiritsandCollectionStickers);
+             }
+
+            MessageBox.Show("New Data Saved ...");
+        }
+
+        private void max_all_Click(object sender, EventArgs e)
+        {
+            gold_box.Text = "9999999";
+            sp_box.Text = "9999999";
+            SkillPoints_Box.Text = "3000";
+            Snack_S_Box.Text = "65535";
+            Snack_M_Box.Text = "65535";
+            Snack_L_Box.Text = "65535";
+            Hammers_Box.Text = "99";
+            tickets_box.Text = "99";
+
+
+        }
+
+    }
+}
